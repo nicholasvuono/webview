2f0709e5ac26c5f7d2aac36d24c02ee98ecdd14b
nvuono <nvuono@outlook.com>
2020-03-31 23:10:30 -0400

    Update webview.go
    Made the webview struct private and added two new fields (port and router), created a constructor method for the webview struct, added a RunAndServe method

--- webview.go
+++ webview.go
@@ -1,19 +1,37 @@
 package webview
 
 import (
+	"net/http"
 	"os/exec"
 	"runtime"
 )
 
-//WebView holds a url string to navigate to.
-type WebView struct {
-	URL string
+//webview holds a url string to navigate. Optionally, you can give it a port and router to serve up.
+type webview struct {
+	url    string
+	port   int
+	router *http.ServeMux
+}
+
+//New is a constructor function for the webview struct.
+func New(url string, port int, router *http.ServeMux) *webview {
+	return &webview{
+		url:    url,
+		port:   port,
+		router: router,
+	}
 }
 
 //Run gets and runs a command to open a browser session.
-func (w *WebView) Run() {
-	webview := getOpenCommand(w.URL)
-	webview.Run()
+func (w *webview) Run() {
+	command := getOpenCommand(w.url)
+	command.Run()
+}
+
+//RunAndServe opens a browser in a go routine and starts up a server.
+func (w *webview) RunAndServe() {
+	go w.Run()
+	http.ListenAndServe(":"+string(w.port), w.router)
 }
 
 //getOpenCommand takes in a url and returns a command to open that url in the default browser.
